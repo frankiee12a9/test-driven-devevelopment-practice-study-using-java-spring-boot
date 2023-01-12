743d25388f0e80077b1702d769faaf7aab2c714b
TKNguyen12a9 <cudayanh@gmail.com>
2023-01-12 17:31:26 +0900

    insert data sql script

---
+++ spring-boot-mvc/tdd/src/main/resources/insertData.sql
@@ -0,0 +1,3 @@
+insert into student(id, first_name, last_name, email_address) values (1, 'jane', 'nguyen', 'mail1@test.com')
+insert into student(id, first_name, last_name, email_address) values (3, 'tom', 'nguyen', 'mail2@test.com')
+insert into student(id, first_name, last_name, email_address) values (5, 'wayne', 'nguyen', 'mail3@test.com')
