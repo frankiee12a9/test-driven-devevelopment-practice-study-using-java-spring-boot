a0e742144fd22caf631b57b318eae5f60753bccf
TKNguyen12a9 <cudayanh@gmail.com>
2023-01-12 17:20:52 +0900

    driver main

---
+++ spring-boot-mvc/tdd/src/main/java/com/mvc/tdd/TddApplication.java
@@ -0,0 +1,46 @@
+package com.mvc.tdd;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+import com.mvc.tdd.entity.*;
+
+// RUN SpringBootApplication
+//  ./mvnw spring-boot:run -e
+
+@SpringBootApplication
+public class TddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddApplication.class, args);
+	}
+
+    @Bean
+	@Scope(value = "prototype")
+	CollegeStudent getCollegeStudent() {
+		return new CollegeStudent();
+	}
+
+	@Bean
+	@Scope(value = "prototype")
+	Grade getMathGrade(double grade) {
+		return new MathGrade(grade);
+	}
+
+	@Bean
+	@Scope(value = "prototype")
+	@Qualifier("mathGrades")
+	MathGrade getGrade() {
+		return new MathGrade();
+	}
+
+	@Bean
+	@Scope(value = "prototype")
+	@Qualifier("scienceGrades")
+	ScienceGrade getScienceGrade() {
+		return new ScienceGrade();
+	}
+}
