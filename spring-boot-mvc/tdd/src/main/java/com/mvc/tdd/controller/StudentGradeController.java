62e41c41c738c81e13237fe9ed29367d4e6a9df1
TKNguyen12a9 <cudayanh@gmail.com>
2023-01-12 18:55:34 +0900

    get student details

--- spring-boot-mvc/tdd/src/main/java/com/mvc/tdd/controller/StudentGradeController.java
+++ spring-boot-mvc/tdd/src/main/java/com/mvc/tdd/controller/StudentGradeController.java
@@ -1,5 +1,7 @@
 package com.mvc.tdd.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.mvc.tdd.entity.CollegeStudent;
 import com.mvc.tdd.entity.Gradebook;
+import com.mvc.tdd.entity.GradebookCollegeStudent;
 import com.mvc.tdd.service.StudentGradeService;
 
 @Controller
@@ -32,12 +35,32 @@ public class StudentGradeController {
 
     @GetMapping(value = "studentDetails/{id}")
     public String studentDetails(@PathVariable int id, Model m) {
+        if (studentGradeService.studentIsNull(id)) return "error";
+
+        Optional<GradebookCollegeStudent> student = studentGradeService.getStudentDetails(id);
+
+        // NOTE: unusual and complicated way to handle this case!
+        // just for testing Optional() features
+        student.ifPresent(e -> {
+            m.addAttribute("student", e);
+
+            boolean eligibleMathGradesCnt = e.getStudentGrades().getMathGradeResults().size() > 0;
+            var mathGrades =  e.getStudentGrades().findGradePointAverage(e.getStudentGrades().getMathGradeResults());
+
+            m.addAttribute("mathGradeAvg", eligibleMathGradesCnt ? mathGrades : "N/A");
+
+            boolean eligibleScienceGradesCnt = e.getStudentGrades().getScienceGradeResults().size() > 0;
+            var scienceGrades =  e.getStudentGrades().findGradePointAverage(e.getStudentGrades().getScienceGradeResults());
+
+            m.addAttribute("scienceGradeAvg", eligibleScienceGradesCnt ? scienceGrades : "N/A");
+        });
+
         return "studentDetails";
     }
 
     @GetMapping(value = "student/delete/{id}")
     public String deleteStudent(@PathVariable int id, Model m) {
-        if (!studentGradeService.studentIsNull(id)) return "error";
+        if (studentGradeService.studentIsNull(id)) return "error";
         
         studentGradeService.deleteStudentById(id);
 
