8d1b1f01e898a5690f369309d50eafce95ddad70
TKNguyen12a9 <cudayanh@gmail.com>
2023-01-27 19:40:35 +0900

    app utils

---
+++ spring-boot-rest-api/movie-api/src/main/java/com/ivanfranchin/movieapi/utils/ApplicationConstants.java
@@ -0,0 +1,9 @@
+package com.ivanfranchin.movieapi.utils;
+
+public class ApplicationConstants {
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+
+	public static final String DEFAULT_PAGE_SIZE = "30";
+
+	public static final int MAX_PAGE_SIZE = 30;
+}
