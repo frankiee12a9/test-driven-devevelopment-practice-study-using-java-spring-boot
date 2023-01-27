5fdb140833450cf78f28dcb4f433d840414ddc8d
TKNguyen12a9 <cudayanh@gmail.com>
2023-01-27 19:52:39 +0900

    tag test

---
+++ spring-boot-rest-api/movie-api/src/test/java/com/ivanfranchin/movieapi/tag/TagServiceTest.java
@@ -0,0 +1,11 @@
+package com.ivanfranchin.movieapi.tag;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@TestPropertySource("/application-test.properties")
+@SpringBootTest
+public class TagServiceTest {
+    
+}
