d745b151e44a4c5b5a923a9c56bfa3b222846409
TKNguyen12a9 <cudayanh@gmail.com>
2023-02-02 15:26:45 +0900

    add app's constants

--- spring-boot-rest-api/movie-api/src/main/java/com/ivanfranchin/movieapi/utils/ApplicationConstants.java
+++ spring-boot-rest-api/movie-api/src/main/java/com/ivanfranchin/movieapi/utils/ApplicationConstants.java
@@ -6,4 +6,26 @@ public class ApplicationConstants {
 	public static final String DEFAULT_PAGE_SIZE = "30";
 
 	public static final int MAX_PAGE_SIZE = 30;
+
+	public static final String USER_ROLE = "ROLE_USER";
+
+	public static final String ADMIN_ROLE = "ROLE_ADMIN";
+
+	public static final String ADMIN = "admin";
+
+	public static final String USER = "user";
+
+	public static final String MOVIE = "movie";
+
+	public static final String MOVIE_TAG = "tag";
+
+	public static final String MOVIE_REVIEW = "movie_review";
+
+	public static final String ID = "id";
+
+	public static final String USERNAME = "username";
+
+	public static final String IMDB = "imdb";
+    
+	public static final String TITLE = "title";
 }
