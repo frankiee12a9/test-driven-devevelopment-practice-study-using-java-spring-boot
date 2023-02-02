e2b8758e91cf8387c6fe473c86d15af838b2a47e
TKNguyen12a9 <cudayanh@gmail.com>
2023-02-02 15:28:55 +0900

    update movie service test

--- spring-boot-rest-api/movie-api/src/test/java/com/ivanfranchin/movieapi/movie/MovieServiceTest.java
+++ spring-boot-rest-api/movie-api/src/test/java/com/ivanfranchin/movieapi/movie/MovieServiceTest.java
@@ -20,10 +20,12 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 
 import com.ivanfranchin.movieapi.model.Movie;
+import com.ivanfranchin.movieapi.model.User;
 import com.ivanfranchin.movieapi.repository.MovieRepository;
 import com.ivanfranchin.movieapi.repository.TagRepository;
-import com.ivanfranchin.movieapi.rest.dto.CreateMovieRequest;
-import com.ivanfranchin.movieapi.rest.dto.CreateMovieResponse;
+import com.ivanfranchin.movieapi.repository.UserRepository;
+import com.ivanfranchin.movieapi.rest.dto.movie.CreateMovieRequest;
+import com.ivanfranchin.movieapi.rest.dto.movie.CreateMovieResponse;
 import com.ivanfranchin.movieapi.service.MovieService;
 import com.ivanfranchin.movieapi.service.MovieServiceImpl;
 
@@ -38,7 +40,6 @@ public class MovieServiceTest {
     @Spy
     @InjectMocks
     @Autowired
-    // private MovieService movieService;
     private MovieService movieService = new MovieServiceImpl(); //? for mockito
 
     @Autowired
@@ -47,8 +48,20 @@ public class MovieServiceTest {
     @Autowired
     private TagRepository tagRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private User user;
+
     @Value("${sql.script.insert.movie}")
     private String sqlInsertMovie;
+
+    @Value("${sql.script.insert.user}")
+    private String sqlInsertUser;
+
+    @Value("${sql.script.delete.users}")
+    private String sqlDeleteUsers;
 
     @Value("${sql.script.delete.movies}")
     private String sqlDeleteMovie;
@@ -63,6 +76,7 @@ public class MovieServiceTest {
 
     @BeforeEach
     void setupDbBeforeTransactions() throws Exception {
+        jdbc.execute(sqlInsertUser);
         jdbc.execute(sqlInsertMovie); 
     }
 
@@ -78,7 +92,12 @@ public class MovieServiceTest {
         var tags = Arrays.asList("test1", "test2");
         createMovieRequest.setTags(tags);
 
-        CreateMovieResponse createMovieResponse =  movieService.saveMovie(createMovieRequest);
+        String username = "Kane";
+        Optional<User> user = userRepository.findByUsername(username);
+
+        assertTrue(user.isPresent(), "should return true");
+
+        CreateMovieResponse createMovieResponse =  movieService.saveMovie(createMovieRequest, user.get());
 
         Movie movieToCheck = movieService.validateAndGetMovie(createMovieResponse.getImdb()); //? just for testing this service's method
 
@@ -105,7 +124,12 @@ public class MovieServiceTest {
 
         assertFalse(movieRepository.findByImdb(createMovieRequest.getImdb()).isPresent(), "Should return false");
 
-        CreateMovieResponse createMovieResponse =  movieService.saveMovie(createMovieRequest);
+        String username = "Kane";
+        Optional<User> user = userRepository.findByUsername(username);
+
+        assertTrue(user.isPresent(), "should return true");
+
+        CreateMovieResponse createMovieResponse =  movieService.saveMovie(createMovieRequest, user.get());
 
         assertTrue(movieRepository.findByImdb(createMovieResponse.getImdb()).isPresent(), "Should return true");
 
@@ -115,9 +139,7 @@ public class MovieServiceTest {
 
         movieService.deleteMovie(validatedMovie);
 
-        validatedMovie = movieService.validateAndGetMovie(validatedMovie.getImdb());
-
-        assertEquals(validatedMovie, null,  "Should return true");
+        assertTrue(movieRepository.findByImdb(validatedMovie.getImdb()).isEmpty(), "Should return true");
     }
 
     @Test
@@ -125,8 +147,13 @@ public class MovieServiceTest {
         CreateMovieRequest createMovieRequest = new CreateMovieRequest(IMDB, TITLE, POSTER);
         createMovieRequest.setTags(ASSOCIATED_TAGS);
 
-        CreateMovieResponse createMovieResponse = movieService.saveMovie(createMovieRequest);
+        String username = "Kane";
+        Optional<User> user = userRepository.findByUsername(username);
 
+        assertTrue(user.isPresent(), "should return true");
+
+        CreateMovieResponse createMovieResponse = movieService.saveMovie(createMovieRequest, user.get());
+        
         assertTrue(tagRepository.findAllInName(createMovieResponse.getTags()).size() == 2, "Should return true");
 
         for (String associatedTag : createMovieResponse.getTags()) 
@@ -137,5 +164,6 @@ public class MovieServiceTest {
     void setupDbAfterTransactions() throws Exception {
         jdbc.execute(sqlDeleteMovieTag); 
         jdbc.execute(sqlDeleteMovie); 
+        jdbc.execute(sqlDeleteUsers);
     }
 }
