8b74cc9f2eca7fdbffabd55c882b990b62dbaf5e
TKNguyen12a9 <cudayanh@gmail.com>
2023-02-02 15:50:13 +0900

    refactor

--- spring-boot-rest-api/movie-api/src/main/java/com/ivanfranchin/movieapi/rest/MovieReviewController.java
+++ spring-boot-rest-api/movie-api/src/main/java/com/ivanfranchin/movieapi/rest/MovieReviewController.java
@@ -2,23 +2,25 @@ package com.ivanfranchin.movieapi.rest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ivanfranchin.movieapi.model.MovieReview;
-import com.ivanfranchin.movieapi.rest.dto.CreateMovieReviewRequest;
-import com.ivanfranchin.movieapi.security.CurrentUser;
+import com.ivanfranchin.movieapi.model.payload.ApiResponse;
+import com.ivanfranchin.movieapi.rest.dto.moviereview.CreateMovieReviewRequest;
+import com.ivanfranchin.movieapi.rest.dto.moviereview.CreateMovieReviewResponse;
 import com.ivanfranchin.movieapi.security.CustomUserDetails;
 import com.ivanfranchin.movieapi.service.MovieReviewService;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.Operation;
-// import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -34,15 +36,34 @@ public class MovieReviewController {
     @Operation(security = {@SecurityRequirement(name = BEARER_KEY_SECURITY_SCHEME)})
     @GetMapping
     public ResponseEntity<Iterable<MovieReview>> getReviews(@PathVariable(name = "movieId") Long movieId) {
+
         Iterable<MovieReview> reviews = reviewService.getReviews(movieId);
         return new ResponseEntity<> (reviews, HttpStatus.OK);
     } 
 
-
     @Operation(security = {@SecurityRequirement(name = BEARER_KEY_SECURITY_SCHEME)})
     @PostMapping
-    public ResponseEntity<MovieReview> addReview(@Valid @RequestBody CreateMovieReviewRequest createMovieReviewRequest, @PathVariable(name ="movieId") Long movieId, @CurrentUser CustomUserDetails customUserDetails) {   
-        MovieReview review = reviewService.addReview(createMovieReviewRequest, movieId, customUserDetails); 
+    public ResponseEntity<CreateMovieReviewResponse> addReview(@Valid @RequestBody CreateMovieReviewRequest createMovieReviewRequest, 
+        @PathVariable(name ="movieId") Long movieId, @AuthenticationPrincipal CustomUserDetails currentUser) {   
+
+        CreateMovieReviewResponse review = reviewService.addReview(createMovieReviewRequest, movieId, currentUser); 
         return new ResponseEntity<> (review, HttpStatus.CREATED);
     }
+
+    @Operation(security = {@SecurityRequirement(name = BEARER_KEY_SECURITY_SCHEME)})
+    @PutMapping("/{id}")
+    public ResponseEntity<MovieReview> editReview(@PathVariable(name = "id") Long id, @Valid @RequestBody MovieReview movieReview) {   
+
+        MovieReview review = reviewService.editReview(movieReview, id); 
+        return new ResponseEntity<> (review, HttpStatus.CREATED);
+    }
+
+    @Operation(security = {@SecurityRequirement(name = BEARER_KEY_SECURITY_SCHEME)})
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse> deleteReview(@PathVariable(name = "id") Long id) {   
+
+        ApiResponse apiResponse = reviewService.deleteReview(id); 
+        return new ResponseEntity<> (apiResponse, HttpStatus.OK);
+    }
 }
+
