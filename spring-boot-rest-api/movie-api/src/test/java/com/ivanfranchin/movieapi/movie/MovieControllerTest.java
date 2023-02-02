2bc2a6fce4e9356f8b50a41d8928d1507f6cc320
TKNguyen12a9 <cudayanh@gmail.com>
2023-02-02 15:26:12 +0900

    update CRUD mocking (read more)
    - add edit movie mocking
    - update mocking @AuthenticationPrincipal with CustomUserDetails endpoint

--- spring-boot-rest-api/movie-api/src/test/java/com/ivanfranchin/movieapi/movie/MovieControllerTest.java
+++ spring-boot-rest-api/movie-api/src/test/java/com/ivanfranchin/movieapi/movie/MovieControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -20,6 +21,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.CoreMatchers.*; // is()
@@ -31,8 +33,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ivanfranchin.movieapi.exception.ErrorResult;
 import com.ivanfranchin.movieapi.model.Movie;
 import com.ivanfranchin.movieapi.repository.MovieRepository;
-import com.ivanfranchin.movieapi.rest.dto.CreateMovieRequest;
-import com.ivanfranchin.movieapi.rest.dto.CreateMovieResponse;
+import com.ivanfranchin.movieapi.rest.dto.movie.CreateMovieRequest;
 import com.ivanfranchin.movieapi.service.MovieService;
 
 import jakarta.persistence.EntityManager;
@@ -62,13 +63,7 @@ public class MovieControllerTest {
     private ObjectMapper objectMapper;
 
     @Autowired
-    private Movie movie;
-
-    @Autowired
     private CreateMovieRequest createMovieRequest;
-
-    @Autowired
-    private CreateMovieResponse createMovieResponse;
 
     @Autowired
     private JdbcTemplate jdbc;   
@@ -89,9 +84,11 @@ public class MovieControllerTest {
 
     private static final int DEFAULT_PAGE_NUMBER = 0;
     private static final int DEFAULT_PAGE_SIZE = 30;
-    private static final int TOTAL_ELEMENTS  = 3;
+    private static final int TOTAL_ELEMENTS  = 1;
     private static final int TOTAL_PAGES = 1;
+    private static final int TOTAL_MOVIES = 10;
     private static final boolean LAST = true;
+
 
     @BeforeAll
     static void setupMock() {
@@ -111,14 +108,15 @@ public class MovieControllerTest {
     @Test
     void getNumberOfMoviesHttpRequest() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/public/numberOfMovies"))
-                .andExpect(status().isOk()).andExpect(jsonPath("$", is(10)));
+                .andExpect(status().isOk()).andExpect(jsonPath("$", is(TOTAL_MOVIES)));
     }
 
     @Test
     @WithMockUser(username = "admin", password = "admin", roles = "ADMIN")
-    void getMovieByTextHttpRequest() throws Exception {
+    void getMovieByGivenTextHttpRequest() throws Exception {
         String text = "home";
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/movies").param("text", text))
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/movies")
+                    .param("text", text))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content").isArray())
                 .andExpect(jsonPath("$.page", is(DEFAULT_PAGE_NUMBER)))
@@ -127,7 +125,7 @@ public class MovieControllerTest {
                 .andExpect(jsonPath("$.totalPages", is(TOTAL_PAGES)))
                 .andExpect(jsonPath("$.last", is(LAST)))
                 .andDo(print());
-            }
+    }
 
     @Test
     @WithMockUser(username = "admin", password = "admin", roles = "ADMIN")
@@ -147,7 +145,6 @@ public class MovieControllerTest {
             .andDo(print())
             .andReturn(); 
 
-        // verify error(exception) response returned from mock
         ErrorResult  expectedErrorResponse = new ErrorResult("imdb", "must not be blank");
         String actualResponse = mvcResult.getResponse().getContentAsString();
         String expectedResponse = objectMapper.writeValueAsString(expectedErrorResponse);
@@ -155,7 +152,7 @@ public class MovieControllerTest {
     }
 
     @Test
-    @WithMockUser(username = "admin", password = "admin", roles = "ADMIN")
+    @WithUserDetails("admin")
     void createMovieHttpRequest() throws Exception {
         String imdb = "tt0163111";
         String title = "today, now";
@@ -192,7 +189,8 @@ public class MovieControllerTest {
         
         mockMvc.perform(MockMvcRequestBuilders.delete("/api/movies/{imdb}", imdb))
                 .andExpect(status().isNotFound())
-                .andDo(print());
+                .andDo(print())
+                .andReturn();
     }
 
     @Test
@@ -207,10 +205,37 @@ public class MovieControllerTest {
                 .andExpect(jsonPath("$.imdb", is(imdb)))
                 .andExpect(jsonPath("$.title", is("No home away"))) 
                 .andExpect(jsonPath("$.poster", is("cudayanh1")))
-                // .andExpect(jsonPath("$.createdAt", is(notNullValue()))) // ? not sure why createdAt sometimes returns null
                 .andDo(print());
             
         assertFalse(movieRepository.findByImdb(imdb).isPresent(), "should return false");
+    }
+
+    @Test
+    @WithUserDetails("user")
+    void editMovieHttpRequest() throws Exception {
+        String newImdb = "tt0163111";
+        String newTitle = "today, now(edit)";
+        String newPoster = "admin";
+        var newAssociatedTags = Arrays.asList("test1", "test2", "test3");
+
+        createMovieRequest.setImdb(newImdb);
+        createMovieRequest.setTitle(newTitle);
+        createMovieRequest.setPoster(newPoster);
+        createMovieRequest.setTags(newAssociatedTags);
+
+        Optional<Movie> movie = movieRepository.findById(6L);
+        assertTrue(movie.isPresent(), "should return true");
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/movies/{id}/edit", movie.get().getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(createMovieRequest))) 
+            .andExpect(status().isCreated())
+            .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+            .andExpect(jsonPath("$.imdb", is(newImdb)))
+            .andExpect(jsonPath("$.title", is(newTitle)))
+            .andExpect(jsonPath("$.poster", is(newPoster)))
+            .andExpect(jsonPath("$.tags", is(newAssociatedTags)))
+            .andDo(print());
     }
 
     @AfterEach
