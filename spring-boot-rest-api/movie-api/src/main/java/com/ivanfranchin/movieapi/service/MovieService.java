bcf1cd92a234539f0148de0e90ced85467696b8c
TKNguyen12a9 <cudayanh@gmail.com>
2023-02-02 15:19:45 +0900

    update CRUD (edit movie)

--- spring-boot-rest-api/movie-api/src/main/java/com/ivanfranchin/movieapi/service/MovieService.java
+++ spring-boot-rest-api/movie-api/src/main/java/com/ivanfranchin/movieapi/service/MovieService.java
@@ -1,10 +1,10 @@
 package com.ivanfranchin.movieapi.service;
 
 import com.ivanfranchin.movieapi.model.Movie;
+import com.ivanfranchin.movieapi.model.User;
 import com.ivanfranchin.movieapi.model.payload.PagedResponse;
-import com.ivanfranchin.movieapi.rest.dto.CreateMovieRequest;
-import com.ivanfranchin.movieapi.rest.dto.CreateMovieResponse;
-
+import com.ivanfranchin.movieapi.rest.dto.movie.CreateMovieRequest;
+import com.ivanfranchin.movieapi.rest.dto.movie.CreateMovieResponse;
 
 public interface MovieService {
 
@@ -14,7 +14,9 @@ public interface MovieService {
 
     Movie validateAndGetMovie(String imdb);
 
-    CreateMovieResponse saveMovie(CreateMovieRequest createMovieRequest);
+    CreateMovieResponse saveMovie(CreateMovieRequest createMovieRequest, User user);
+
+    CreateMovieResponse editMovie(Long id, CreateMovieRequest editMovieRequest, User user);
 
     void deleteMovie(Movie movie);
 }
