69c5a979279dfaea22d0c4a183251010de670cb5
TKNguyen12a9 <cudayanh@gmail.com>
2023-02-08 11:27:47 +0900

    add editMovie

--- spring-boot-rest-api/movie-api/src/main/java/com/ivanfranchin/movieapi/service/MovieServiceImpl.java
+++ spring-boot-rest-api/movie-api/src/main/java/com/ivanfranchin/movieapi/service/MovieServiceImpl.java
@@ -1,15 +1,16 @@
 package com.ivanfranchin.movieapi.service;
 
-import com.ivanfranchin.movieapi.exception.BadRequestException;
-import com.ivanfranchin.movieapi.exception.MovieNotFoundException;
 import com.ivanfranchin.movieapi.exception.ResourceNotFoundException;
 import com.ivanfranchin.movieapi.model.Movie;
 import com.ivanfranchin.movieapi.model.Tag;
+import com.ivanfranchin.movieapi.model.User;
 import com.ivanfranchin.movieapi.model.payload.PagedResponse;
 import com.ivanfranchin.movieapi.repository.MovieRepository;
 import com.ivanfranchin.movieapi.repository.TagRepository;
-import com.ivanfranchin.movieapi.rest.dto.CreateMovieRequest;
-import com.ivanfranchin.movieapi.rest.dto.CreateMovieResponse;
+import com.ivanfranchin.movieapi.repository.UserRepository;
+import com.ivanfranchin.movieapi.rest.dto.movie.CreateMovieRequest;
+import com.ivanfranchin.movieapi.rest.dto.movie.CreateMovieResponse;
+import com.ivanfranchin.movieapi.security.CustomUserDetails;
 import com.ivanfranchin.movieapi.utils.ApplicationConstants;
 import com.ivanfranchin.movieapi.utils.ApplicationUtils;
 
@@ -24,7 +25,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-// @RequiredArgsConstructor
 @Service
 public class MovieServiceImpl implements MovieService {
 
@@ -65,39 +65,70 @@ public class MovieServiceImpl implements MovieService {
 
     @Override
     public Movie validateAndGetMovie(String imdb) {
-        return movieRepository.findByImdb(imdb).orElse(null);
+        return movieRepository.findByImdb(imdb)
+                    .orElseThrow(() -> new ResourceNotFoundException("Movie", "imdb", imdb));
     }
 
     @Override
-    public CreateMovieResponse saveMovie(CreateMovieRequest createMovieRequest) {
+    public CreateMovieResponse saveMovie(CreateMovieRequest createMovieRequest, User currentUser) {
         List<String> tagNames = createMovieRequest.getTags();
-        // List<Tag> tags = tagRepository.findAllInName(tagNames);
         List<Tag> tags = new ArrayList<>(tagNames.size());
         for (String name : tagNames) {
             Tag tag = tagRepository.findByName(name).orElse(null);
 			tag = tag == null ? tagRepository.save(new Tag(name)) : tag;
             tags.add(tag);
         }
-    
+
         Movie movie = new Movie();
         movie.setImdb(createMovieRequest.getImdb());
         movie.setTitle(createMovieRequest.getTitle());
         movie.setPoster(createMovieRequest.getPoster());
+        movie.setUser(currentUser);
         movie.setTags(tags);
 
         Movie savedMovie = movieRepository.save(movie);
 
-        CreateMovieResponse createMovieResponse = new CreateMovieResponse();
-        createMovieResponse.setImdb(savedMovie.getImdb());
-        createMovieResponse.setTitle(savedMovie.getTitle());
-        createMovieResponse.setPoster(savedMovie.getPoster());
-        createMovieResponse.setTags(savedMovie.getTags().stream().map(tag -> tag.getName()).toList());
+        CreateMovieResponse response = new CreateMovieResponse();
+        response.setImdb(savedMovie.getImdb());
+        response.setTitle(savedMovie.getTitle());
+        response.setPoster(savedMovie.getPoster());
+        response.setTags(savedMovie.getTags().stream().map(tag -> tag.getName()).toList());
         
-        return createMovieResponse;
+        return response;
     }
 
     @Override
     public void deleteMovie(Movie movie) {
         movieRepository.delete(movie);
+    }
+
+    @Override
+    public CreateMovieResponse editMovie(Long id, CreateMovieRequest editMovieRequest, User user) {
+        Movie movieToEdit = movieRepository.findById(id).orElseThrow(
+            () -> new ResourceNotFoundException("Movie", "id", id));
+        List<String> tagNames = new ArrayList<>(editMovieRequest.getTags());
+        List<Tag> tags = new ArrayList<>();
+        for (String name : tagNames) {
+            Tag tag = tagRepository.findByName(name).orElse(null);
+			tag = tag == null ? tagRepository.save(new Tag(name)) : tag;
+            tags.add(tag);
+        }
+        movieToEdit.setImdb(editMovieRequest.getImdb());
+        movieToEdit.setTitle(editMovieRequest.getTitle());
+        movieToEdit.setPoster(editMovieRequest.getPoster());
+        movieToEdit.setTags(tags);
+        movieToEdit.setUser(user);
+
+        Movie savedMovie = movieRepository.save(movieToEdit);
+
+        var response  = new CreateMovieResponse();
+        response.setImdb(savedMovie.getImdb());
+        response.setTitle(savedMovie.getTitle());
+        response.setPoster(savedMovie.getPoster());
+        response.setCreatedBy(savedMovie.getCreatedBy());
+        response.setUpdatedAt(savedMovie.getUpdatedAt());
+        response.setTags(savedMovie.getTags().stream().map(tag -> tag.getName()).toList());
+
+        return response;
     }
 }
